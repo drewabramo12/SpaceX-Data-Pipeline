ac61b90232f00c2578415455a73ab029c80fdf58
Andrew Abramo <drewabramo12@gmail.com>
2024-11-09 21:29:41 -0800

    Added tap file for spacex api and config

---
+++ tap-spacex/tap_spacex.py
@@ -0,0 +1,24 @@
+import singer
+import pandas as pd
+
+LOGGER = singer.get_logger()
+
+schema = {
+    'properties': {
+        'id': {'type': 'string'},
+        'name': {'type': 'string'},
+        'date_utc':{'type': 'string', 'format': 'date-time'},
+    }
+}
+
+def main():
+    url = 'https://api.spacexdata.com/v4/launches'
+    df = pd.read_json(url)
+
+    records = df.to_dict(orient='records')
+
+    singer.write_schema('launches', schema, 'id')
+    singer.write_records('launches', records)
+
+    if __name__ == '__main__':
+        main()
